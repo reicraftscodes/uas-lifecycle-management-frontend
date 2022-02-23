b4315076e831722597395c9f5bbc2e095728892c
Oliver Holdaway <holdawayo@cardiff.ac.uk>
2022-02-23 16:45:01 +0000

    feat : issue #10 switched part type to drop down menu and part status to radio buttons

--- src/pages/AddPart.js
+++ src/pages/AddPart.js
@@ -1,5 +1,8 @@
+import { faBold } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
-import {Form, FormGroup, Label, Input,Button} from "reactstrap";
+import ReactDOM from "react-dom";
+import {Form, FormGroup, Label, Input,Button, ButtonGroup} from "reactstrap";
+import '../css/AddPart.css'
 
 const AddPart = () => {
     const [partType, setPartType] = useState('');
@@ -18,17 +21,37 @@ const AddPart = () => {
             headers: {"Content-Type": "application/json" },
             body: JSON.stringify(part)
         }).then(response => response.json()).then(data => console.log(data))
+
+        
     }
 
+
     return (
+
+        
+
         <div className="addPart">
             <h1>Add Part</h1>
 
             <Form>
                 <FormGroup>
                     <Label for="partTypeForm">Part Type</Label>
-                    <Input id="partTypeForm" name="Part Type" type="text" value={partType} onChange={(e) => setPartType(e.target.value)}/>
+                    <Input id="partTypeForm" name="Part Type" type="select" value={partType} onChange={(e) => setPartType(e.target.value)}>
+                        <option value="1">Wing A</option>
+                        <option value="2">Wing B</option>
+                        <option value="3">Fuselage</option>
+                        <option value="4">Tail</option>
+                        <option value="5">Propeller</option>
+                        <option value="6">Motor</option>
+                        <option value="7">Communications Radio</option>
+                        <option value="8">Payload Electo Optical</option>
+                        <option value="9">Payload Infra-Red</option>
+                        <option value="10">Quad Arm</option>
+                        <option value="11">Gimble</option>
+                        
+                    </Input>
                 </FormGroup>
+
                 <FormGroup>
                     <Label for="aircraftForm">Aircraft</Label>
                     <Input id="aircraftForm" name="Aircraft" type="text" value={aircraft} onChange={(e) => setAircraft(e.target.value)}/>
@@ -37,19 +60,27 @@ const AddPart = () => {
                     <Label for="locationForm">Location</Label>
                     <Input id="locationForm" name="Location" type="text" value={location} onChange={(e) => setLocation(e.target.value)}/>
                 </FormGroup>
-                <FormGroup>
-                    <Label for="partStatusForm">Part Status</Label>
-                    <Input id="partStatusForm" name="Part Status" type="text" value={partStatus} onChange={(e) => setPartStatus(e.target.value)}/>
+
+                <FormGroup class="radioButton" tag="fieldset" value={partStatus} checked={partStatus}>
+                    <Input value="OPERATIONAL" name="partStatusForm" type="radio" onChange={(e) => setPartStatus(e.target.value)}/>
+                    <Label check>Operational</Label>
+
+                    <Input value="AWAITING_REPAIR" name="partStatusForm" type="radio" onChange={(e) => setPartStatus(e.target.value)}/>
+                    <Label check>Awaiting Repair</Label>
+
+                    <Input value="BEING_REPAIRED" name="partStatusForm" type="radio" onChange={(e) => setPartStatus(e.target.value)}/>
+                    <Label check>Being Repaired</Label>
+                    
+                    <Input value="BEYOND_REPAIR" name="partStatusForm" type="radio" onChange={(e) => setPartStatus(e.target.value)}/>
+                    <Label check>Beyond Repair</Label>
+                    
                 </FormGroup>
-
-                <Button onClick={handleSubmission}>Submit</Button>
             </Form>
+        </div>
 
 
-        </div>  
     );
 }
 
-    
 
 export default AddPart
