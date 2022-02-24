394159f59d730a61934b51f5ae3232ecd4df7d06
Oliver Holdaway <holdawayo@cardiff.ac.uk>
2022-02-24 10:22:11 +0000

    feat : issue #10 changes labels and moved form into grid layout

--- src/pages/AddPart.js
+++ src/pages/AddPart.js
@@ -1,7 +1,7 @@
 import { faBold } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 import ReactDOM from "react-dom";
-import {Form, FormGroup, Label, Input,Button, ButtonGroup} from "reactstrap";
+import {Form, FormGroup, Label, Input,Button, ButtonGroup, Col, Container,Alert} from "reactstrap";
 import '../css/AddPart.css'
 
 const AddPart = () => {
@@ -21,65 +21,79 @@ const AddPart = () => {
              headers: {"Content-Type": "application/json" },
              body: JSON.stringify(part)
          }).then(response => response.json()).then(data => console.log(data))
-
-        
-
         
     }
 
 
     return (
-
-        
-
         <div className="addPart">
-            <h1>Add Part</h1>
 
+            <div class="heading">
+               <h1>Add Part</h1> 
+            </div>
+
+            <div class="form">
             <Form>
-                <FormGroup>
-                    <Label for="partTypeForm">Part Type</Label>
-                    <Input id="partTypeForm" name="Part Type" type="select" value={partType} onChange={(e) => setPartType(e.target.value)}>
-                        <option value="1">Wing A</option>
-                        <option value="2">Wing B</option>
-                        <option value="3">Fuselage</option>
-                        <option value="4">Tail</option>
-                        <option value="5">Propeller</option>
-                        <option value="6">Motor</option>
-                        <option value="7">Communications Radio</option>
-                        <option value="8">Payload Electo Optical</option>
-                        <option value="9">Payload Infra-Red</option>
-                        <option value="10">Quad Arm</option>
-                        <option value="11">Gimble</option>
+                <FormGroup row>
+                    <Label for="partTypeForm" sm={2}>Part Type</Label>
+
+                    <Col sm={10}>
+                        <Input id="partTypeForm" name="Part Type" type="select" value={partType} onChange={(e) => setPartType(e.target.value)}>
+                            <option value="1">Wing A</option>
+                            <option value="2">Wing B</option>             
+                            <option value="3">Fuselage</option>
+                            <option value="4">Tail</option>
+                            <option value="5">Propeller</option>
+                            <option value="6">Motor</option>
+                            <option value="7">Communications Radio</option>
+                            <option value="8">Payload Electo Optical</option>
+                            <option value="9">Payload Infra-Red</option>
+                            <option value="10">Quad Arm</option>
+                            <option value="11">Gimble</option>
+                        </Input>
+                    </Col>
+                </FormGroup>
+
+                <FormGroup row>
+                    <Label for="aircraftForm" sm={2}>Aircraft</Label>
+
+                    <Col sm={10}>
+                        <Input w-25 id="aircraftForm" name="Aircraft" type="text" value={aircraft} onChange={(e) => setAircraft(e.target.value)}/>
+                    </Col>
+                </FormGroup>
+
+                <FormGroup row>
+                    <Label for="locationForm" sm={2}>Location</Label>
+
+                    <Col sm={10}>
+                        <Input id="locationForm" name="Location" type="text" value={location} onChange={(e) => setLocation(e.target.value)}/>
+                    </Col>
+                </FormGroup>
+
+                <FormGroup row class="radioButton" tag="fieldset" value={partStatus} checked={partStatus}>
+                    <Label sm={2}>Part status</Label>
+
+                    <Col sm={10}>
+                        <Input value="OPERATIONAL" name="partStatusForm" type="radio" onChange={(e) => setPartStatus(e.target.value)}/>
+                        <Label class="radioLabel" check>Operational</Label>
+
+                        <Input value="AWAITING_REPAIR" name="partStatusForm" type="radio" onChange={(e) => setPartStatus(e.target.value)}/>
+                        <Label class="radioLabel" check>Awaiting Repair</Label>
+
+                        <Input value="BEING_REPAIRED" name="partStatusForm" type="radio" onChange={(e) => setPartStatus(e.target.value)}/>
+                        <Label class="radioLabel" check>Being Repaired</Label>
                         
-                    </Input>
+                        <Input value="BEYOND_REPAIR" name="partStatusForm" type="radio" onChange={(e) => setPartStatus(e.target.value)}/>
+                        <Label class="radioLabel" check>Beyond Repair</Label>
+                    </Col>
                 </FormGroup>
 
-                <FormGroup>
-                    <Label for="aircraftForm">Aircraft</Label>
-                    <Input id="aircraftForm" name="Aircraft" type="text" value={aircraft} onChange={(e) => setAircraft(e.target.value)}/>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="locationForm">Location</Label>
-                    <Input id="locationForm" name="Location" type="text" value={location} onChange={(e) => setLocation(e.target.value)}/>
-                </FormGroup>
-
-                <FormGroup class="radioButton" tag="fieldset" value={partStatus} checked={partStatus}>
-                    <Input value="OPERATIONAL" name="partStatusForm" type="radio" onChange={(e) => setPartStatus(e.target.value)}/>
-                    <Label check>Operational</Label>
-
-                    <Input value="AWAITING_REPAIR" name="partStatusForm" type="radio" onChange={(e) => setPartStatus(e.target.value)}/>
-                    <Label check>Awaiting Repair</Label>
-
-                    <Input value="BEING_REPAIRED" name="partStatusForm" type="radio" onChange={(e) => setPartStatus(e.target.value)}/>
-                    <Label check>Being Repaired</Label>
-                    
-                    <Input value="BEYOND_REPAIR" name="partStatusForm" type="radio" onChange={(e) => setPartStatus(e.target.value)}/>
-                    <Label check>Beyond Repair</Label>
-                    
-                </FormGroup>
-
-                <Button onClick={handleSubmission}>Submit</Button>
+                <Col sm={{offset: 2, size: 10}}>
+                    <Button onClick={handleSubmission}>Submit</Button>
+                </Col>
             </Form>
+            </div>
+
         </div>
 
         
