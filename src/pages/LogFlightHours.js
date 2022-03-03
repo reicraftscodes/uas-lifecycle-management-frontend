1e2cac6ad90704601ac34fb8d5daff91bf2e749a
Oliver Holdaway <holdawayo@cardiff.ac.uk>
2022-03-03 20:16:05 +0000

    feat : issue #13 Added alert messages for errors and ensured post requests arent sent if there is an error

--- src/pages/LogFlightHours.js
+++ src/pages/LogFlightHours.js
@@ -1,10 +1,16 @@
-import {Autocomplete, Button, Select,TextField, FormControl} from "@mui/material";
+import {Autocomplete, Button, Select,TextField, FormControl, Paper, Alert, Divider} from "@mui/material";
 import React, { useEffect, useState } from 'react';
+import '../css/AddPart.css'
 
 const LogFlightHours = () => {
-    const aircrafts = [];
+    //const aircrafts = [];
+    const [aircrafts, setAircrafts] = useState([]);
     const [aircraft, setAircraft] = useState("");
-    const [flightHours, setFlightHours] = useState("");
+    const [flyTime, setFlightHours] = useState("");
+
+    const [alert, setAlert] = useState(false);
+    const [alertMessage, setAlertMessage] = useState('');
+    const [alertSeverity, setAlertSeverity] = useState('success');
 
     useEffect(() => {
         fetch('http://localhost:8080/aircraft/user/2', {
@@ -18,25 +24,68 @@ const LogFlightHours = () => {
     }, []);
 
     const onSubmit = () => {
-        console.log(aircraft);
-        console.log(flightHours);
+
+        const request = {aircraft,flyTime};
+
+        if (aircraft == ""){
+            setAlertMessage("Aircraft field cannot be blank.");
+            setAlertSeverity("error");
+            setAlert(true);
+            setTimeout(() => { setAlert(false) }, 3000);
+        } else if (flyTime == "" || flyTime < 0){
+            setAlertMessage("Flight hours cannot be blank or negative.");
+            setAlertSeverity("error");
+            setAlert(true);
+            setTimeout(() => { setAlert(false) }, 3000);
+        } else {
+            fetch('http://localhost:8080/aircraft/log-flight', {
+                method: 'POST',
+                headers: {"Content-Type": "application/json" },
+                body: JSON.stringify(request)
+            }).then(data => {
+                //if the request is successful an alert is shown with a success message
+                setAlertMessage("Flight hours logged!");
+                setAlertSeverity("success");
+                setAlert(true);
+                //hides the alert after displaying for 3 seconds
+                setTimeout(() => { setAlert(false) }, 3000);
+            }).catch(error => { 
+                //catches error for not being able to communicate with the server and displays an alert to the user.
+                setAlertMessage("Error communicating with server, part not saved");
+                setAlertSeverity("error");
+                setAlert(true);
+                setTimeout(() => { setAlert(false) }, 3000);
+            });
+        }
+
     }
-
-
 
     return (
         <div className="body">
-            <h1>Log Flight Hours</h1>
+            <Paper elevation={3} sx={{width: "65%", margin: "auto", p: "3%", pt: "0%", mt: "1%" }}>
+                    <div className="alertPos">
+                        {alert ? <Alert severity={alertSeverity}>{alertMessage}</Alert> : <></> }
+                    </div> 
+                <h1>Log Flight Hours</h1>
+                <Divider/>
+                <br/>
+                <FormControl>
+                    <Autocomplete options={aircrafts} onChange={(event, newValue) => {setAircraft(newValue)}} renderInput={(params) => (
+                        <TextField {...params} label="Aircraft" variant="outlined" />
+                    )} />
+                    <br/>
+                    <Divider/>
+                    <br/>
+                    <TextField label="Flight Hours" type="number" onChange={(e) => setFlightHours(e.target.value)}></TextField>
+                    <br/>
+                    <Divider/>
+                    <br/>
+                    <Button variant="contained" onClick={onSubmit}>Submit</Button>
+                </FormControl>
+            </Paper>
+            
 
-            <FormControl>
-                <Autocomplete options={aircrafts} onChange={(event, newValue) => {setAircraft(newValue)}} renderInput={(params) => (
-                    <TextField {...params} label="Aircraft" variant="outlined" />
-                )} />
 
-                <TextField label="Flight Hours" type="number" onChange={(e) => setFlightHours(e.target.value)}></TextField>
-
-                <Button variant="contained" onClick={onSubmit}>Submit</Button>
-            </FormControl>
 
             
 
