78dbfa8f9fdc7983d76bfed60381164281c1a710
Oliver Holdaway <holdawayo@cardiff.ac.uk>
2022-03-03 19:25:41 +0000

    feat : issue #13 Added form to the page and stored user inputs in state variables

--- src/pages/LogFlightHours.js
+++ src/pages/LogFlightHours.js
@@ -1,32 +1,47 @@
-import {Button} from "@mui/material";
-import React, { useEffect } from 'react';
+import {Autocomplete, Button, Select,TextField, FormControl} from "@mui/material";
+import React, { useEffect, useState } from 'react';
 
 const LogFlightHours = () => {
+    const aircrafts = [];
+    const [aircraft, setAircraft] = useState("");
+    const [flightHours, setFlightHours] = useState("");
 
     useEffect(() => {
-        const aircraft = [];
-
         fetch('http://localhost:8080/aircraft/user/2', {
             method: 'GET',
             headers: {"Content-Type": "application/json" }
         }).then(response => response.json()).then(data => {
             for(let i = 0; i < data.length; i++){
-                aircraft[i] = data[i].tailNumber;
+                aircrafts[i] = data[i].tailNumber;
             }
-
         });
-
-        console.log(aircraft);
-        
-
     }, []);
+
+    const onSubmit = () => {
+        console.log(aircraft);
+        console.log(flightHours);
+    }
 
 
 
     return (
         <div className="body">
             <h1>Log Flight Hours</h1>
-            <Button>Click</Button>
+
+            <FormControl>
+                <Autocomplete options={aircrafts} onChange={(event, newValue) => {setAircraft(newValue)}} renderInput={(params) => (
+                    <TextField {...params} label="Aircraft" variant="outlined" />
+                )} />
+
+                <TextField label="Flight Hours" type="number" onChange={(e) => setFlightHours(e.target.value)}></TextField>
+
+                <Button variant="contained" onClick={onSubmit}>Submit</Button>
+            </FormControl>
+
+            
+
+
+
         </div>
 
         
