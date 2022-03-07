543b3643c57f53a1e354a42bb6a2427c46454d19
May <sanejolm@cardiff.ac.uk>
2022-03-07 12:14:08 +0000

    fix: issue #14 fix user login form

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -1,50 +1,54 @@
 import React, {useEffect, useState} from 'react';
-import {useHistory} from "react-router-dom";
-import {Button, Checkbox, FormControlLabel, Grid, Link, Paper, TextField, Typography} from "@mui/material";
+import {Button, Divider, FormControl, Grid, Link, Paper, TextField, Typography} from "@mui/material";
 import Logo from '../assets/logo.png';
+// import {useHistory} from "react-router-dom";
 
 
 // Login Page functionality
 function UserLogin() {
 
     //custom paper style
-    const paperStyle={padding :20,height:'70vh',width:280, margin:"20px auto"}
-    //
-    // // user input state variables
-    // const [session, setSession] = useState()
-    // const [username, setUsername] = useState('');
-    // const [password, setPassword] = useState('');
-    // const [roles, setRoles] = useState('');
+    const paperStyle={padding :20,height:'70vh',width:380, margin:"20px auto"}
+
+    // user input state variables
+    const [session, setSession] = useState()
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [roles, setRoles] = useState('');
     // const history = useHistory();
-    //
-    // // Get Login form
-    // const getLoginHeader = () => {
-    //
-    //     //TODO Calls getLogin service
-    //
-    // }
-    //
-    // // UseEffect functionality
-    // useEffect( () => {
-    //
-    //
-    // }, [] )
+
+    // Get Login form
+    const getLoginHeader = () => {
+
+        //TODO Calls getLogin service
+
+    }
+
+    // UseEffect functionality
+    useEffect( () => {
+
+
+    }, [] )
 
     return(
         <Grid>
             <Paper elevation={4} style={paperStyle}>
                 <Grid align='center'>
-                    <img src={Logo} id="sncLogo"/>
-                    <h2>Sign In</h2>
+                    <img src={Logo} id="sncLogo" height="50px" width="150px" />
+                    <h4>Sign In</h4>
                 </Grid>
-                <TextField label='Email' placeholder='Enter your email' fullWidth required/>
-                <TextField label='Password' placeholder='Enter your password' type='password' fullWidth required/>
-                <Button type='submit' color='primary' variant="contained" > fullWidth>Sign in</Button>
+                <FormControl>
+                <TextField label='Email' placeholder='Enter your email'/>
+                <br/>
+                <TextField label='Password' placeholder='Enter your password' type='password'/>
+                <br/>
+                <Button type='submit' color='primary' variant="contained" > Login </Button><br/>
                 <Typography >
                     <Link href="#" >
                         Forgot password ?
                     </Link>
                 </Typography>
+                </FormControl>
             </Paper>
         </Grid>
     )
