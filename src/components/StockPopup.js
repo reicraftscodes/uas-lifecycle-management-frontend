f56495ea9fb1b9aaed853a8968a802bca22be86b
Joe Bailey <baileyj18@cardiff.ac.uk>
2022-03-14 18:09:04 +0000

    Feat: #3 Added new alert based on prototype, currently uses dummy data but fetch is present, needs to be displayed on LODashboard.

--- src/components/StockPopup.js
+++ src/components/StockPopup.js
@@ -1,12 +1,45 @@
 import React from 'react';
-import { UncontrolledAlert } from 'reactstrap';
+import {Button, Card, CardText, CardTitle} from 'reactstrap';
+import WarningAmberIcon from '@mui/icons-material/WarningAmber';
 
-function StockPopup() {
-    return (
-        <UncontrolledAlert color="danger">
-            <a href="/stock-levels" id="alertText">Low Stock Levels!</a>
-        </UncontrolledAlert>
+export default class StockPopup extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {lowStockParts: []};
+    }
+
+    componentDidMount() {
+        //fetch api used to send get request
+        fetch('http://localhost:8080/parts/low-stock')
+            .then(response => response.json()).then(data => {
+            this.setState({lowStockParts: data})
+        }).catch(error => {
+            //catches error for not being able to communicate with the server and displays an alert to the user.
+            alert("Server request for low stock parts failed.")
+        })
+    }
+
+    render() {
+
+        const {lowStockParts} = this.state;
+
+        //const alertMessage = lowStockParts.length;
+
+        //Using dummy data temporarily.
+        const alertMessage = 12;
+
+        console.log(lowStockParts);
+
+        return (
+            <Card>
+                <CardTitle id="alertTitle">Stock Level Warning</CardTitle>
+                <CardText>{alertMessage}</CardText>
+                <Button id="view-location">View Location</Button>
+                <WarningAmberIcon id="warningIcon"></WarningAmberIcon>
+            </Card>
+
     );
+    }
 }
-
-export default StockPopup;
