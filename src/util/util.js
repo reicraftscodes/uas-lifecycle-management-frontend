df87fee5073dee6ba5cf825bf916ececcebb7371
May <sanejolm@cardiff.ac.uk>
2022-03-16 13:59:09 +0000

    fix: issue #14 changed user route util.js

--- src/util/util.js
+++ src/util/util.js
@@ -1,14 +1,14 @@
 export const getUserDashboard = (role) => {
     switch (role) {
         case "ROLE_USER_LOGISTIC":
-            return '/logistic-dashboard';
+            return '/logistics-officer/dashboard';
         case "ROLE_USER":
-            return '/user-dashboard';
+            return '/user-aircraft';
         case "ROLE_USER_CTO":
             return '/cto-dashboard';
         case 'ROLE_USER_CEO':
             return '/ceo-dashboard'
         case 'ROLE_USER_COO':
-            return '/coo-dashboard'
+            return '/stock-levels'
     }
 }
