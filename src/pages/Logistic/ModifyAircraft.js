ceefb4b2a02037b5998bb398c2843be388ca9649
Oliver Holdaway <holdawayo@cardiff.ac.uk>
2022-03-17 19:40:29 +0000

    feat : issue #25 Displayed data in table dynamically

--- src/pages/Logistic/ModifyAircraft.js
+++ src/pages/Logistic/ModifyAircraft.js
@@ -1,5 +1,5 @@
 
-import {Paper, Button, TextField, FormControl} from "@mui/material";
+import {Paper, Button, TextField, FormControl, TableRow, TableCell, TableContainer, Table, TableHead, TableBody, Grid} from "@mui/material";
 import React, {useState} from "react";
 import AircraftService from "../../services/AircraftService";
 
@@ -21,13 +21,56 @@ const ModifyAircraft = () => {
         <div>
             <Paper elevation={3} sx={{width: "95%", margin: "auto", p: "3%", pt: "0%", mt: 2 }}>
                 <h1>Modify Aircraft</h1>
-                <FormControl>
-                    <TextField label="Aircraft tailnumber" onChange={(e) => setTailNumber(e.target.value)} ></TextField>
-                    <br/>
-                    <Button variant="contained" onClick={onAircraftSearch}>Search Aircraft</Button>
-                </FormControl>
+                
+                <Grid container>
+                    <Grid item xs={3}>
+                        <FormControl>
+                            <TextField label="Aircraft tailnumber" onChange={(e) => setTailNumber(e.target.value)} ></TextField>
+                            <br/>
+                            <Button variant="contained" onClick={onAircraftSearch}>Search Aircraft</Button>
+                        </FormControl>
+                    </Grid>
 
-                <p>{parts[0][1]}</p>
+                    <Grid item xs={9}>
+                                            
+                        <TableContainer sx={{width: "50%"}}>
+                            <Table size="small" aria-label="table label">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Part Number</TableCell>
+                                        <TableCell>Part Name</TableCell>
+                                        <TableCell>Part Status</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {parts.map((row) => (
+                                        <TableRow key={row[0]}>
+                                            <TableCell sx={{fontSize: "0.7rem"}}>{row[0]}</TableCell>
+                                            <TableCell sx={{fontSize: "0.7rem"}}>{row[1]}</TableCell>
+                                            <TableCell sx={{fontSize: "0.7rem"}}>{row[2]}</TableCell>
+                                        </TableRow>
+
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Grid>
+
+
+
+                </Grid>
+
+                    
+
+
+                    
+
+                
+
+                
+                
+
+                
 
 
             </Paper>
