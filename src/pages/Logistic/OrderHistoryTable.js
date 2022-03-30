562e3534b824e20d7569ea98e5927de678d5a597
May Sanejo <sanejolm@cardiff.ac.uk>
2022-03-30 12:40:06 +0100

    feat: #38 Logistic officer now able to see the order history in table, he can filter the table if logistic wants to.

---
+++ src/pages/Logistic/OrderHistoryTable.js
@@ -0,0 +1,160 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import TextField from '@mui/material/TextField';
+import {DataGrid} from '@mui/x-data-grid';
+import ClearIcon from '@mui/icons-material/Clear';
+import SearchIcon from '@mui/icons-material/Search';
+import {createStyles, makeStyles} from "@mui/styles";
+
+function HistoryQuickSearchToolbar(props) {
+    return (
+        <Box
+            sx={{
+                p: 0.5,
+                pb: 0,
+            }}
+        >
+            <TextField
+                variant="standard"
+                value={props.value}
+                onChange={props.onChange}
+                placeholder="Search…"
+                InputProps={{
+                    startAdornment: <SearchIcon fontSize="small"/>,
+                    endAdornment: (
+                        <IconButton
+                            title="Clear"
+                            aria-label="Clear"
+                            size="small"
+                            style={{visibility: props.value ? 'visible' : 'hidden'}}
+                            onClick={props.clearSearch}
+                        >
+                            <ClearIcon fontSize="small"/>
+                        </IconButton>
+                    ),
+                }}
+                sx={{
+                    width: {
+                        xs: 1,
+                        sm: 'auto',
+                    },
+                    m: (theme) => theme.spacing(1, 0.5, 1.5),
+                    '& .MuiSvgIcon-root': {
+                        mr: 0.5,
+                    },
+                    '& .MuiInput-underline:before': {
+                        borderBottom: 1,
+                        borderColor: 'divider',
+                    },
+                }}
+            />
+        </Box>
+    );
+}
+
+HistoryQuickSearchToolbar.propTypes = {
+    clearSearch: PropTypes.func.isRequired,
+    onChange: PropTypes.func.isRequired,
+    value: PropTypes.string.isRequired,
+};
+
+function OrderHistoryTable({data}) {
+    function escapeRegExp(value) {
+        return value.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+    }
+
+    function attachIdToToRow(data) {
+        return data.map((element, index) => ({...element, id: index}));
+    }
+
+    const [searchText, setSearchText] = React.useState('');
+    const [rows, setRows] = React.useState(attachIdToToRow(data));
+
+    const requestSearch = (searchValue) => {
+        setSearchText(searchValue);
+        const searchRegex = new RegExp(escapeRegExp(searchValue), 'i');
+        const filteredRows = data.filter((row) => {
+            return Object.keys(row).some((field) => {
+                return searchRegex.test(row[field].toString());
+            });
+        });
+        setRows(attachIdToToRow(filteredRows));
+    };
+
+    const columns = [
+        {
+            field: 'locationName',
+            headerName: 'Location Name',
+            flex: 1,
+            headerClassName: 'grid-header-mui'
+        },
+        {
+            field: 'supplierEmail',
+            headerName: 'Supplier Email',
+            flex: 1,
+            headerClassName: 'grid-header-mui'
+        },
+
+        {
+            field: 'totalCost',
+            headerName: 'Total Cost (£)',
+            flex: 1,
+            headerClassName: 'grid-header-mui'
+        },
+        {
+            field: 'orderDateTime',
+            headerName: 'Order Date Time',
+            flex: 1,
+            headerClassName: 'grid-header-mui'
+        },
+        {
+            field: 'partName',
+            headerName: 'Part Name',
+            flex: 1,
+            headerClassName: 'grid-header-mui'
+        },
+
+        {
+            field: 'quantity',
+            headerName: 'Quantity',
+            type: 'number',
+            flex: 1,
+            headerClassName: 'grid-header-mui'
+        },
+    ];
+
+    const useStyles = makeStyles((theme) =>
+        createStyles({
+            root: {
+                "& .MuiDataGrid-columnHeaders": {
+                    backgroundColor: "#808080",
+                    fontSize: 16
+                }
+            }
+        })
+    );
+
+    const classes = useStyles();
+
+    return (
+        <Box sx={{height: 700, width: '100%'}}>
+            <DataGrid
+                components={{Toolbar: HistoryQuickSearchToolbar}}
+                rows={rows}
+                columns={columns}
+                componentsProps={{
+                    toolbar: {
+                        value: searchText,
+                        onChange: (event) => requestSearch(event.target.value),
+                        clearSearch: () => requestSearch(''),
+                    },
+                }}
+                className={classes.root}
+            />
+        </Box>
+    );
+}
+
+export default OrderHistoryTable;
---
+++ src/pages/Logistic/ViewHistoryOrders.js
@@ -0,0 +1,53 @@
+import React, {useEffect, useState} from 'react';
+import {Box, Grid, Paper, styled} from "@mui/material";
+import PartsService from "../../services/PartsService";
+import OrderHistoryTable from "./OrderHistoryTable";
+import Typography from "@mui/material/Typography";
+
+
+function ViewHistoryOrders() {
+    const [historyOrders, setHistoryOrders] = useState([]);
+
+
+    useEffect(() => {
+        getOrderHistory();
+    }, []);
+
+    const getOrderHistory = () => {
+        PartsService.getOrderHistory()
+            .then(response => response.json())
+            .then(data => {
+                console.log("Successfully retrieved stock history", data);
+                setHistoryOrders(data);
+                console.log(data);
+            })
+            .catch(error => {
+                console.log("Error retrieving stock history", error);
+            })
+    }
+
+    const Item = styled(Paper)(({theme}) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    }));
+
+    return (
+        <div>
+            <Box sx={1} margin="10px">
+                <Typography variant="h3">Order History</Typography>
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <Item>
+                            <OrderHistoryTable data={historyOrders}/>
+                        </Item>
+                    </Grid>
+                </Grid>
+            </Box>
+        </div>
+    );
+}
+
+export default ViewHistoryOrders;
