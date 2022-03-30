6952c843b3778b829e1bd57d1772d77783e6f938
May Sanejo <sanejolm@cardiff.ac.uk>
2022-03-30 11:53:25 +0100

    refactor: #38 removed unused import and put the gitlab ci back

--- src/pages/Logistic/ViewPartsStockLocations.js
+++ src/pages/Logistic/ViewPartsStockLocations.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
-import ViewListPartsTable from "../ViewPartsStockLocation]/ViewListPartsTable";
 import PartsService from "../../services/PartsService";
 import StockPartsLocation from "../../components/StockPartsLocation";
 import Box from "@mui/material/Box";
